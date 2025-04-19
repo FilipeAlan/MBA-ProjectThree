13d23ca1e915a847ea87d1fbb4b6b66a1cbd27ea
Filipe Alan <filipealan@gmail.com>
2025-04-19 18:42:58 -0300

    mapeamento estava comentado

--- src/EducPlatform/CursoContext.Infraestructure/Mappings/CursoMap.cs
+++ src/EducPlatform/CursoContext.Infraestructure/Mappings/CursoMap.cs
@@ -8,35 +8,35 @@ public class CursoMap : IEntityTypeConfiguration<Curso>
 {
     public void Configure(EntityTypeBuilder<Curso> builder)
     {
-        //builder.ToTable("Cursos");
+        builder.ToTable("Cursos");
 
-        //builder.HasKey(c => c.Id);
+        builder.HasKey(c => c.Id);
 
-        //builder.Property(c => c.Nome)
-        //    .IsRequired()
-        //    .HasMaxLength(150);
+        builder.Property(c => c.Nome)
+            .IsRequired()
+            .HasMaxLength(150);
 
-        //builder.OwnsOne(c => c.Conteudo, conteudo =>
-        //{
-        //    conteudo.Property(p => p.Descricao)
-        //        .HasColumnName("ConteudoDescricao")
-        //        .IsRequired()
-        //        .HasMaxLength(1000);
+        builder.OwnsOne(c => c.Conteudo, conteudo =>
+        {
+            conteudo.Property(p => p.Descricao)
+                .HasColumnName("ConteudoDescricao")
+                .IsRequired()
+                .HasMaxLength(1000);
 
-        //    conteudo.Property(p => p.Objetivos)
-        //        .HasColumnName("ConteudoObjetivos")
-        //        .IsRequired()
-        //        .HasMaxLength(1000);
-        //});
+            conteudo.Property(p => p.Objetivos)
+                .HasColumnName("ConteudoObjetivos")
+                .IsRequired()
+                .HasMaxLength(1000);
+        });
 
-        //builder.HasMany(c => c.Aulas)
-        //    .WithOne()
-        //    .HasForeignKey("CursoId")
-        //    .OnDelete(DeleteBehavior.Cascade);
+        builder.HasMany(c => c.Aulas)
+            .WithOne()
+            .HasForeignKey("CursoId")
+            .OnDelete(DeleteBehavior.Cascade);
 
-        //builder.Property(c => c.DataCriacao);
-        //builder.Property(c => c.DataAtualizacao);
-        //builder.Property(c => c.UsuarioCriacao).IsRequired();
-        //builder.Property(c => c.UsuarioAtualizacao).IsRequired();
+        builder.Property(c => c.DataCriacao);
+        builder.Property(c => c.DataAtualizacao);
+        builder.Property(c => c.UsuarioCriacao).IsRequired();
+        builder.Property(c => c.UsuarioAtualizacao).IsRequired();
     }
 }
